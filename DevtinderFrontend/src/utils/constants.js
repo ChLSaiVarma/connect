4db6d3ef9f163e7135e5fb488da90adf690f5b76
saivarma <chlsaivarma186@gmail.com>
2025-03-08 23:24:43 +0530

    Base url issue

--- DevtinderFrontend/src/utils/constants.js
+++ DevtinderFrontend/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://connect-backend-eta.vercel.app/" || "http://localhost:7777";
+export const BASE_URL = "https://connect-backend-eta.vercel.app" || "http://localhost:7777";
